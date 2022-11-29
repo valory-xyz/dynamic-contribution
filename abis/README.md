e5f8736fe2e80ef26950ba19813f6933568f7dff
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-11-29 18:41:08 +0000

    chore: adding ABIs

---
+++ abis/README.md
@@ -0,0 +1,2 @@
+# Autonolas dynamic distribution ABIs
+These ABIs were obtained with 1000000 optimization passes.
--- hardhat.config.js
+++ hardhat.config.js
@@ -55,7 +55,7 @@ module.exports = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 750,
+                        runs: 1000000,
                     },
                 },
             }
