6f15cbf1d324a67afff9d91f99e730304774547e
David Minarsch <david.minarsch@googlemail.com>
2024-03-16 19:50:22 -0700

    chore: update to 0.8.23

--- contracts/DynamicContribution.sol
+++ contracts/DynamicContribution.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.23;
 
 import "../lib/solmate/src/tokens/ERC721.sol";
 import "../lib/solmate/src/utils/LibString.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -51,7 +51,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.17",
+                version: "0.8.23",
                 settings: {
                     optimizer: {
                         enabled: true,
